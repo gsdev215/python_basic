10069e33c4649a36b30ff660bd88695c81799ed9
gsdev215 <74264993+gsdev215@users.noreply.github.com>
2021-01-01 18:21:07 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,65 @@
+#first example to print a string value 
+
+'''input'''
+
+print('hello world')
+
+# if its a string value/a word and
+#sentence we use ' in after oping and closing 
+#bracket in python 
+
+'''output'''
+
+#hello world
+
+#second example to pring a number
+
+print (10)
+
+#incase of a integer/number we do not use '
+
+'''output'''
+
+#10
+
+#third example how to print a variable 
+
+#we store or declare a  variable like this
+
+'''input'''
+
+name='dev bhattacharjee'
+print (name)
+
+#we store/write  a string/word in ' because
+#-if we don't write it in this way it will think 
+#-that it is a variable and say no variable in name
+#-of this is declared 
+
+'''output'''
+#dev bhattacharjee
+
+
+# fourth example 
+
+'''input'''
+
+num1=1
+num2=4
+
+sum=num1+num2
+#this is how we add in python
+
+print ('sum of ',num1,'and',num2,'is',sum)
+#we print multiple words and variable together
+#-by just give a , in between them
+
+'''output'''
+
+#sum of 1 and 4 is 5
+
+
+'''hey now u know basic of print command'''
+'''u can move to next file'''
+
+
