a72910975ba3c697f3de2190549542a3543f33df
gsdev215 <74264993+gsdev215@users.noreply.github.com>
2021-02-25 21:24:47 +0530

    Add files via upload

---
+++ chapter1/variable.py
@@ -0,0 +1,81 @@
+#variables for one of important thing in python
+#variables basically store values 
+
+# example 1
+
+A=1
+
+print (A)
+
+'''output'''
+
+# 1
+
+
+# example 2
+
+A=1
+B=2
+
+sum=A+B
+
+print (sum)
+
+'''output'''
+# 3
+
+# example 3 storing a string value 
+
+A='hi am dev bhattacharjee'
+
+print (A) 
+
+'''output'''
+
+# hi am dev bhattacharjee 
+
+# example 4 printing multiple value using variable 
+
+A='I am '
+F='dev bhattacharjee'
+G='Rahul Das'
+
+print (A , F)  #to print multiple value u must give a [ ,] between variable or string 
+print (A , G) #see how it save my time
+
+'''output'''
+
+# I am dev bhattacharjee 
+# I am Rahul Das
+
+# example 5 cross changing value[basic / comman]
+
+A=1
+B=2
+
+C=A #her we said that A is equal to C or 1 is equal to C
+B=C #her we changed to value of B and now its 1
+
+print (B)
+
+'''output'''
+
+# 1
+
+# example 6 cross changing value  [ advance one]
+
+A=1
+B=2
+
+A,B=B,A #here we made A value is shifted to B and B value is shifted to A
+
+print (B)
+print (A)
+
+'''output'''
+
+# 1
+# 2
+
+'''now u can mobe forward to next topic'''
+
